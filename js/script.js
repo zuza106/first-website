43ac0f7ec7daef34b5df861e05a95b8394821c52
Zuzanna Skorupka <zuza106@gmail.com>
2023-11-29 07:11:22 +0100

    changes in removeWelcomeText and in secondButton

--- js/script.js
+++ js/script.js
@@ -4,29 +4,31 @@
         console.log("Cześć! To moja pierwsza wiadomość.");
     }
 
-    const removeTextButton = () => {
-        const welcomeText = document.querySelector(".js-welcomeText");
-        welcomeText.remove();
-    }
 
 
-
-    const changeTextButton = () => {
-        const secondButton = document.querySelector(".js-secondButton");
+    const changeText = () => {
         const secondText = document.querySelector(".js-secondText");
-        secondButton.innerText = secondText.innerText === "Moje hobby"
+        secondText.innerText = secondText.innerText === "Moje hobby" ? "Moje pasje" : "Moje hobby";
+
+    }
+    const changeTextButton = (changeText) => {
+        const secondButton = document.querySelector(".js-secondButton");
+        secondButton.innerText = changeText.innerText === "Moje hobby"
             ? "Zmień tekst nagłówka"
             : "Zmień tekst ponownie";
     }
 
-    const changeText = () => {
-        secondText.innerText = secondText.innerText === "Moje hobby" ? "Moje pasje" : "Moje hobby";
+    
 
-    }
+    const removeWelcomeText = () => {
+        const welcomeText = document.querySelector(".js-welcomeText");
+        welcomeText.remove();
+    };
 
     const init = () => {
         const button = document.querySelector(".js-button");
-        removeTextButton.addEventListener("click")
+
+        button.addEventListener("click", removeWelcomeText);
         secondButton.addEventListener("click", changeTextButton, changeText)
 
 
